6c9c026334fba5c01193216a0ac2db394e0dfd13
Wizzy-PC <wizzy83@gmail.com>
2020-10-04 14:41:05 +0200

    imports

--- stocks_direction.py
+++ stocks_direction.py
@@ -1,4 +1,14 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import tensorflow.keras as tf
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dense, Dropout
+from tensorflow.keras.optimizers import Adam, SGD
 
 
-for x in range(50):
-    print(x)
+pd.set_option('display.width', 400)
+pd.set_option('display.max_columns', 40)
+sns.set_style('whitegrid')
